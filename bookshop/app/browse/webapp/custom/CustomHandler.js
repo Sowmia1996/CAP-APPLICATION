9924fbac5695d1380fd27027c364758e932f600f
Sowmia <m.sowmia@sap.com>
2023-12-22 18:38:12 +0530

    sixth commit

--- bookshop/app/browse/webapp/custom/CustomHandler.js
+++ bookshop/app/browse/webapp/custom/CustomHandler.js
@@ -1,38 +1,71 @@
-sap.ui.define(["sap/m/MessageBox", "sap/ui/model/json/JSONModel"], function (MessageBox, JSONModel) {
+sap.ui.define(["sap/m/MessageBox", "sap/ui/core/Fragment", "sap/ui/model/Filter"], function (MessageBox, Fragment, Filter) {
     "use strict";
 
     return {
         onChange: function (oEvent) {
-            const sBookId = oEvent.getSource().getParent().getParent().getBindingContext().getProperty('ID')
-            const iQuantity = oEvent.getParameters().value
-            MessageBox.show(`Book id: ${sBookId} and quantity: ${iQuantity}}`)
+            
             // Try to store the quantity information in the odata model itself like below
             // oEvent.getSource().getBindingContext().setproperty() --> Note that odata can only be accessed using: sap.ui.model.odata.v4.Context
             // Then later you can access this in the createorder function for each book that is selected
         },
 
         createOrder: async function() {
-            const aSelectedBooksContext = this.getSelectedContexts()
-            const aPayloadItems = aSelectedBooksContext.map(oBook=> {
-                return {
-                    bookId: oBook.getProperty('ID'),
-                    quantity: 2
-                }
-            })
-            // Call the unbound action with these payloads
-            const oModel = this.getModel();
-            const oActionODataContextBinding = oModel.bindContext("/createOrder(...)");
-            oActionODataContextBinding.setParameter("items", aPayloadItems);
-            oActionODataContextBinding.setParameter("address", "Hard-Coded Address For testing");
-            oActionODataContextBinding.execute().then(
-                function() {
-                    const oActionContext = oActionODataContextBinding.getBoundContext();
-                    console.table(oActionContext.getObject().value);
-                }.bind(this)
-            );
+            const oBooksListPage = sap.ui.getCore().byId("browseBooks::BooksList")
+            if(!this.oCartItemsDialog) {
+                this.oCartItemsDialogId = `${oBooksListPage.getId()}-CartItemsDialog`;
+                this.oCartItemsDialog = await Fragment.load({
+                    id: this.oCartItemsDialogId,
+                    name: "browseBooks.custom.CartItemsDialog"
+                });
+                oBooksListPage.addDependent(this.oCartItemsDialog)
+                this.oCartItemsDialog.setModel(this.getModel(), 'mine')
+            }
+
+            this.oCartItemsDialog.attachBeforeOpen(() => {
+                const oList = Fragment.byId(this.oCartItemsDialogId, "cartItems");
+                const aSelectedBooksId = this.getSelectedContexts().map((oContext)=> oContext.getProperty('ID'))
+                const aListFilter = [];
+                aSelectedBooksId.forEach((id) => aListFilter.push(new Filter('ID', 'EQ', id)))
+                oList.bindAggregation('items', {
+                    path: 'mine>/Books', 
+                    model: 'mine', 
+                    template: oList.removeItem(0),
+                    filters: new Filter({filters: aListFilter, and: false})
+                });
+            });
+
+            this.oCartItemsDialog.open();
+
+            // ----------------------To be moved to a different handler ------------------------------------//
+            // const aSelectedBooksContext = this.getSelectedContexts()
+            // const aPayloadItems = aSelectedBooksContext.map(oBook=> {
+            //     return {
+            //         bookId: oBook.getProperty('ID'),
+            //         quantity: 2
+            //     }
+            // })
+            // // Call the unbound action with these payloads
+            // const oModel = this.getModel();
+            // const oActionODataContextBinding = oModel.bindContext("/createOrder(...)");
+            // oActionODataContextBinding.setParameter("items", aPayloadItems);
+            // oActionODataContextBinding.setParameter("address", "Hard-Coded Address For testing");
+            // oActionODataContextBinding.execute().then(
+            //     function() {
+            //         const oActionContext = oActionODataContextBinding.getBoundContext();
+            //         console.table(oActionContext.getObject().value);
+            //     }.bind(this)
+            // );
         },
 
-        onPress: function () {
+        submit: function (oEvent) {
+            oEvent.getSource().getParent().close();
+        },
+
+        cancel: function (oEvent) {
+            oEvent.getSource().getParent().close();
+        },
+
+        onPress: function (oEvent) {
             MessageBox.show("Hi, this isn't implemented yet!");
             //const sBookId = oEvent.getSource().getParent().getParent().getBindingContext().getProperty('ID')
         }
